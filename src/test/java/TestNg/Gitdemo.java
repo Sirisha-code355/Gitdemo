328fae8d935b33d376282bfa06a76e12093263ce
sirisha <mandalasirisha3@gmail.com>
2024-09-01 19:34:50 +0530

    commit

--- src/test/java/TestNg/Gitdemo.java
+++ src/test/java/TestNg/Gitdemo.java
@@ -4,8 +4,9 @@ public class Gitdemo {
 
 	public static void main(String[] args) {
 	System.out.println("Print 1");
-	System.out.println("Print 1");
+	System.out.println("Print 2");
 
+	System.out.println("Print 3");
 	}
 
 }
